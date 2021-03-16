172da1cae0dcb5d31f97251e23aeb417923df9d2
Rob Dustin <RDustin@newbelgium.com>
2021-03-16 16:01:18 -0600

    usings

--- DotNetShipping/Helpers/Extensions/RequestedShipmentExtensions.cs
+++ DotNetShipping/Helpers/Extensions/RequestedShipmentExtensions.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
+﻿
 using ShippingProviderCostHelper.RateServiceWebReference;
 
 namespace ShippingProviderCostHelper.Helpers.Extensions
